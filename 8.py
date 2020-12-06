3b704d8d35cad59b78709c722fff01e11a34c6eb
alimoatasem <alimoatasem2000@yahoo.com>
2020-12-06 18:55:27 +0200

    change made

--- 8.py
+++ 8.py
@@ -1,4 +1,13 @@
-print("hello")
-print("bbbbbbb")
-print("boi")
-print("x")
+a=[[0,0,0],
+   [0,0,0],
+   [0,0,0]]
+
+for i in range(3):
+    for j in range(3):
+        a[i][j]=int(input("Enter number: "))
+
+for i in range(3):
+    for j in range(3):
+        print(a[i][j], end=" ")
+
+
