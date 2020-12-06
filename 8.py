4553ec3eaf4312f1489a7a5b4659ef2a5be005c4
alimoatasem <alimoatasem2000@yahoo.com>
2020-12-06 19:12:16 +0200

    change made

--- 8.py
+++ 8.py
@@ -1,6 +1,12 @@
 a=[[0,0,0],
    [0,0,0],
    [0,0,0]]
+for i in range(3):
+    for j in range(3):
+        a[i][j]=int(input("Enter number: "))
 
-for j in range(3):
-    a[j]=int(input("Enter number: "))
+for i in range(3):
+    for j in range(3):
+        print(a[i][j], end=" ")
+
+
